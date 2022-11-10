e51e12fe0ed9086ce746c277a6f4f24f64a33db3
LabibaMashiat20 <labibamashiatmahi@gmail.com>
2022-11-10 07:04:26 +0600

    footer added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,8 +1,23 @@
+import Main from "../../Layout/Main";
+import Blogs from "../../Paths/Blogs/Blogs";
+import Home from "../../Paths/Home/Home";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router=createBrowserRouter([
     {
-        
+        path: '/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/blogs',
+                element:<Blogs></Blogs>
+            }
+        ]
     }
 ]);
 export default router;
